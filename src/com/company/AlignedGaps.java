b6d59e451f400683d4665e2bb1880a4cd820ed88
jroberts3136 <jroberts3136@gmail.com>
2017-12-06 08:52:15 -0500

    Possibly Finished

--- src/com/company/AlignedGaps.java
+++ src/com/company/AlignedGaps.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AlignedGaps {
@@ -21,37 +23,70 @@ public class AlignedGaps {
         Must work for any number of cases [3,100]
             Will always be true for 2 or fewer cases
          */
-        int numCases = cases;
-        double xDiff = coordinates_x[2] - coordinates_x [1];
-        double yDiff = coordinates_y[2] - coordinates_y[1];
-        double zDiff = coordinates_z[2] - coordinates_z[1];
+
+        double xDiff = coordinates_x[1] - coordinates_x[0];
+        double yDiff = coordinates_y[1] - coordinates_y[0];
+        double zDiff = coordinates_z[1] - coordinates_z[0];
         double r;
         final double EPSILON = 0.00001;
+        boolean collinear = false;
 
-        if (numCases < 3) {
-            return true;
+
+
+
+        if (cases <= 2) {
+            collinear = true;
         }
+
         else{
-            for (int i = 3; i <= numCases; i++){
-                if (Math.abs(xDiff) > Math.abs(yDiff) && Math.abs(xDiff) > Math.abs(zDiff)){
-                    r = (co)
+            for (int i = 2; i < cases; i++){
+                if ((Math.abs(xDiff) > Math.abs(yDiff) && Math.abs(xDiff) > Math.abs(zDiff)) || (Math.abs(xDiff) == Math.abs(yDiff) && Math.abs(xDiff) == Math.abs(yDiff))){
+                    r = (coordinates_x[i] - coordinates_x[0])/xDiff;
+
+                    if (Math.abs((coordinates_y[i]-coordinates_y[0]) - (r*yDiff)) < EPSILON   &&
+                            Math.abs((coordinates_z[i]-coordinates_z[0]) - (r*zDiff)) < EPSILON){
+
+                        collinear = true;
+                    }
+                    else{
+                        collinear = false;                          //The points cannot be collinear if any one is not collinear with the rest
+                        break;
+                    }
                 }
 
                 if (Math.abs(yDiff) > Math.abs(xDiff) && Math.abs(yDiff) > Math.abs(zDiff)){
+                    r = (coordinates_y[i] - coordinates_y[0])/yDiff;
 
+                    if (Math.abs((coordinates_x[i]-coordinates_x[0]) - (r*xDiff)) < EPSILON  &&
+                            Math.abs((coordinates_z[i]-coordinates_z[0]) - (r*zDiff)) < EPSILON){
+                        collinear = true;
+                    }
+                    else{
+                        collinear = false;
+                        break;
+                    }
                 }
 
                 if (Math.abs(zDiff) > Math.abs(xDiff) && Math.abs(zDiff) > Math.abs(yDiff)){
+                    r = (coordinates_z[i] - coordinates_z[0])/zDiff;
 
+                    if (Math.abs((coordinates_x[i]-coordinates_x[0]) - (r*xDiff)) < EPSILON  &&
+                            Math.abs((coordinates_y[i]-coordinates_y[0]) - (r*yDiff)) < EPSILON){
+                        collinear = true;
+                    }
+                    else{
+                        collinear = false;
+                        break;
+                    }
                 }
             }
         }
 
-
+        return collinear;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(new File("01.in"));
 
         int numCases = sc.nextInt();
 
@@ -67,7 +102,7 @@ public class AlignedGaps {
                 coordinates_z[i] = sc.nextDouble();
             }
 
-            if(areGapsAligned(coordinates_x, coordinates_y, coordinates_z, numCases)) {
+            if(areGapsAligned(coordinates_x, coordinates_y, coordinates_z, num)) {
                 System.out.println("Gaps are aligned.");
             } else {
                 System.out.println("Gaps are NOT aligned.");
@@ -77,3 +112,6 @@ public class AlignedGaps {
         sc.close();
     }
 }
+/*
+
+ */
